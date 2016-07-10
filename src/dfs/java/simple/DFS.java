72cc5591699f6c21e10dfc2f22028dcc056f089e
ClaasM <claasmeiners@googlemail.com>
2016-07-10 21:57:53 +0200

    Implemented BFS and made some cosmetic changes to dfs

--- src/dfs/java/simple/DFS.java
+++ src/dfs/java/simple/DFS.java
@@ -2,7 +2,7 @@ package dfs.java.simple;
 
 /**
  * Created by ClaasM on 7/9/2016.
- * Used to perform Depth-First-Search (DFS) using adjacency matrizes.
+ * Used to perform Depth-First-Search (DFS) using adjacency matrices.
  * For a faster implementation, see @see ../fast/DFS.java (using adjacency Lists)
  */
 public class DFS {
@@ -10,7 +10,7 @@ public class DFS {
      * Recursive implementation of Depth-First-Search using adjacency matrix.
      * This returns nothing (yet), it is meant to be a template for whatever you want to do with it,
      * e.g. finding connected components in a graph.
-     * This has a runtime of O(n^2), for a faster implementation see @see ../fast/DFS.java (using adjacency Lists)
+     * This has a runtime of O(|V|^2) (|V| = number of Nodes), for a faster implementation see @see ../fast/DFS.java (using adjacency Lists)
      * @param graph an adjacency-matrix-representation of the graph where (x,y) is true if the the there is an edge between nodes x and y.
      * @param current the node to start from.
      * @param visited an array denoting whether a node has been visited or not.
