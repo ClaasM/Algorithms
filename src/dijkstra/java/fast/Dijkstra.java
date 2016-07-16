e7c6a8095ef49e74259a07305790d4b6afc611c2
ClaasM <claasmeiners@googlemail.com>
2016-07-16 23:21:42 +0200

    Fixed some smaller errors, created the class for fast dijkstra (not yet implemented)

---
+++ src/dijkstra/java/fast/Dijkstra.java
@@ -0,0 +1,22 @@
+package dijkstra.java.fast;
+
+/**
+ * Created by claas on 7/11/2016.
+ * Used to perform the dijkstra Algorithm using adjacency lists.
+ * For a faster implementation, see @see ../fast/Dijkstra.java (using adjacency Lists)
+ */
+public class Dijkstra {
+    /**
+     * Implementation of dijkstra using adjacency matrix & binary heap.
+     * This returns an array containing the length of the shortest path from the start node to each other node.
+     * It is only guaranteed to return correct results if there are no negative edges in the graph. Positive cycles are fine.
+     * This has a runtime of O(|E| + |V|log|V|) (|V| = number of Nodes, |E| = number of Edges), for a simpler implementation see @see ../simple/Dijkstra.java (using adjacency matrices)
+
+     * @param start the node to start from.
+     */
+    public static int[] dijkstra(int start) {
+        //TODO implement using binary heap
+
+        return null;
+    }
+}
--- src/dijkstra/java/simple/Dijkstra.java
+++ src/dijkstra/java/simple/Dijkstra.java
@@ -13,10 +13,11 @@ public class Dijkstra {
     /**
      * Implementation of dijkstra using adjacency matrix.
      * This returns an array containing the length of the shortest path from the start node to each other node.
-     * It is only guaranteed to return correct results if there are no negative edges in the graph. Positive cycles are fine. //TODO check if this is true
-     * This has a runtime of O(|V|^2) (|V| = number of Nodes), for a faster implementation see @see ../fast/Dijkstra.java (using adjacency Lists)
+     * It is only guaranteed to return correct results if there are no negative edges in the graph. Positive cycles are fine.
+     * This has a runtime of O(|V|^2) (|V| = number of Nodes), for a faster implementation see @see ../fast/Dijkstra.java (using adjacency lists)
      * @param graph an adjacency-matrix-representation of the graph where (x,y) is the weight of the edge or 0 if there is no edge.
      * @param start the node to start from.
+     * @return an array containing the shortest distances from the given start node to each other node
      */
     public static int[] dijkstra(int[][] graph, int start) {
 
