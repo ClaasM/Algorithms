bb399b7bfd35c7f7e8409b9523b140818d138dc9
ClaasM <claasmeiners@googlemail.com>
2016-07-17 23:55:40 +0200

    Implemented Point in Polygon

---
+++ src/point_in_polygon/java/PointInPolygon.java
@@ -0,0 +1,31 @@
+package point_in_polygon.java;
+
+/**
+ * Created by claas on 7/17/2016.
+ * Used to perform the Raycasting Algorithm to find out whether a point is in a given polygon.
+ */
+public class PointInPolygon {
+    /**
+     * Performs the even-odd-rule Algorithm to find out whether a point is in a given polygon.
+     * This runs in O(n) where n is the number of edges of the polygon.
+     * @param polygon an array representation of the polygon where polygon[i][0] is the x Value of the i-th point and polygon[i][1] is the y Value.
+     * @param point an array representation of the point where point[0] is its x Value and point[1] is its y Value
+     * @return whether the point is in the polygon (not on the edge, just turn < into <= and > into >= for that)
+     */
+    public static boolean pointInPolygon(int[][] polygon, int[] point) {
+        //A point is in a polygon if a line from the point to infinity crosses the polygon an odd number of times
+        boolean odd = false;
+        //For each edge (In this case for each point of the polygon and the previous one)
+        for (int i = 0, j = polygon.length - 1; i < polygon.length; i++) {
+            //If a line from the point into infinity crosses this edge
+            if (((polygon[i][1] > point[1]) != (polygon[j][1] > point[1]))
+                    && (point[0] < (polygon[j][0] - polygon[i][0]) * (point[1] - polygon[i][1]) / (polygon[j][1] - polygon[i][1]) + polygon[i][0])) {
+                //Invert odd
+                odd = !odd;
+            }
+            j = i;
+        }
+        //If the number of crossings was odd, the point is in the polygon
+        return odd;
+    }
+}
