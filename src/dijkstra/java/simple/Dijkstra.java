e4e13513cb70ef7cf301fd3ed05facec563769df
Claas Meiners <ga96sag@tum.de>
2019-09-01 14:00:47 +0200

    Fixed Dijkstra

--- src/dijkstra/java/simple/Dijkstra.java
+++ src/dijkstra/java/simple/Dijkstra.java
@@ -1,5 +1,7 @@
 package dijkstra.java.simple;
 
+import javafx.util.Pair;
+
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -15,6 +17,7 @@ public class Dijkstra {
      * This returns an array containing the length of the shortest path from the start node to each other node.
      * It is only guaranteed to return correct results if there are no negative edges in the graph. Positive cycles are fine.
      * This has a runtime of O(|V|^2) (|V| = number of Nodes), for a faster implementation see @see ../fast/Dijkstra.java (using adjacency lists)
+     *
      * @param graph an adjacency-matrix-representation of the graph where (x,y) is the weight of the edge or 0 if there is no edge.
      * @param start the node to start from.
      * @return an array containing the shortest distances from the given start node to each other node
@@ -22,29 +25,47 @@ public class Dijkstra {
     public static int[] dijkstra(int[][] graph, int start) {
 
         //This contains the distances from the start node to all other nodes
-        int[] distance = new int[graph.length];
+        int[] distances = new int[graph.length];
         //Initializing with a distance of "Infinity"
-        Arrays.fill(distance, Integer.MAX_VALUE);
+        Arrays.fill(distances, Integer.MAX_VALUE);
+
+        //This contains whether a node was already visited
+        boolean[] visited = new boolean[graph.length];
 
         //The distance from the start node to itself is of course 0
-        distance[start] = 0;
+        distances[start] = 0;
 
-        //A Queue to manage the nodes that have yet to be visited
-        Queue<Integer> queue = new PriorityQueue<>();
+        //Wether we have already visited a node
 
         //While there are nodes left to visit...
-        while (!queue.isEmpty()) {
-            int node = queue.remove();
-            //...for all neighboring nodes that haven't been visited yet....
-            for (int i = 0; i < graph[node].length; i++) {
-                //...if the path over this edge is shorter...
-                if (graph[node][i] != 0 && distance[i] > distance[node] + graph[node][i]) {
-                    //...Save this path as new shortest path.
-                    distance[i] = distance[node] + graph[node][i];
+        while (true) {
+
+            // ... find the node with the currently shortest distance from the start node...
+            int shortestDistance = Integer.MAX_VALUE;
+            int shortestIndex = -1;
+            for (int i = 0; i < graph.length; i++) {
+                //... by going through all nodes that haven't been visited yet
+                if (distances[i] < shortestDistance && !visited[i]) {
+                    shortestDistance = distances[i];
+                    shortestIndex = i;
                 }
             }
-        }
-        return distance;
 
+            if (shortestIndex == -1) {
+                // There was no node not yet visited --> We are done
+                return distances;
+            }
+
+            //...then, for all neighboring nodes that haven't been visited yet....
+            for (int i = 0; i < graph[shortestIndex].length; i++) {
+                //...if the path over this edge is shorter...
+                if (graph[shortestIndex][i] != 0 && distances[i] > distances[shortestIndex] + graph[shortestIndex][i]) {
+                    //...Save this path as new shortest path.
+                    distances[i] = distances[shortestIndex] + graph[shortestIndex][i];
+                }
+            }
+            // Lastly, note that we are finished with this node.
+            visited[shortestIndex] = true;
+        }
     }
 }
