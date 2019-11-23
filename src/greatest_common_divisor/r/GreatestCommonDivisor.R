d13f1a4a68721953dfcac298e226d5e4acdb13e2
Claas Meiners <ga96sag@tum.de>
2019-11-23 15:50:15 +0100

    R implementation of GCD

---
+++ src/greatest_common_divisor/r/GreatestCommonDivisor.R
@@ -0,0 +1,19 @@
+# Used to perform the Euclidean Algorithm to find the greatest common divisor (gcd) of two numbers.
+# For more than two numbers, e.g. three, you can box it like this: gcd(a,gcd(b,greatest_common_divisor.c)) etc.
+
+# Recursive implementation to find the gcd (greatest common divisor) of two integers using the euclidean algorithm.
+# This runs in O(log(n)) where n is the maximum of a and b.
+# @param a the first integer
+# @param b the second integer
+# @return the greatest common divisor (gcd) of the two integers.
+gcd <- function (a,b) {
+  print(sprintf("New *a* is %s, new *b* is %s",a,b))
+  if(b == 0){
+    print(sprintf("b is 0, stopping recursion, a is the gcd: %s", a))
+    return (a)
+  }
+  print(sprintf("Recursing with new a = b and new b = a %% b..."))
+  gcd(b, a %% b)
+}
+
+print(gcd(10,20))
