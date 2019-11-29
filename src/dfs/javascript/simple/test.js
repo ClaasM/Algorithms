0ef9248ad4ceccea6a85629937bd118d6e46a600
Claas Meiners <ga96sag@tum.de>
2019-11-29 23:55:45 +0100

    implemented dfs in js

---
+++ src/dfs/javascript/simple/test.js
@@ -0,0 +1,23 @@
+const {dfs} = require('./dfs')
+
+// Smaller graph used for dijkstra
+var start = {
+    value: 0, children: [
+        {
+            value: 1, children: [
+                {value: 3, children: {}},
+                {value: 4, children: {}}
+            ]
+        }, {
+            value: 2, children: [
+                {value: 5, children: {}},
+                {value: 6, children: {}}
+            ]
+        }]
+}
+
+// Bigger graph specifically for this algorithm
+// TODO
+
+// Should be 6
+console.log(dfs(start, 6).value);
