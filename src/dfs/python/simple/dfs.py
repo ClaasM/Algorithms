f224e5afcbc60127274fd4faf112659050f0d06e
Claas Meiners <ga96sag@tum.de>
2019-12-01 16:02:56 +0100

    implemented dfs in python

---
+++ src/dfs/python/simple/dfs.py
@@ -0,0 +1,33 @@
+"""
+Created by claas on 7/17/2016.
+Used to perform the Depth-First Search (DFS) Algorithm to find the shortest path from a start to a target node.
+ """
+
+"""
+Implementation of DFS (depth-first search).
+Given a start node, this returns the node in the tree below the start node with the target value (or null if it doesn't exist)
+Runs in O(n), where n is the number of nodes in the tree, or O(b^d), where b is the branching factor and d is the depth.
+ *
+@param start  the node to start the search from
+@param target the value to search for
+@return The node containing the target value or null if it doesn't exist.
+"""
+
+
+def dfs(start, target):
+    print("Visiting Node " + str(start["value"]))
+    if start["value"] == target:
+        # We have found the goal node we we're searching for
+        print("Found the node we're looking for!")
+        return start
+
+    # Recurse with all children
+    for i in range(len(start["children"])):
+        result = dfs(start["children"][i], target)
+        if result is not None:
+            # We've found the goal node while going down that child
+            return result
+
+    # We've gone through all children and not found the goal node
+    print("Went through all children of " + str(start["value"]) + ", returning to it's parent.")
+    return None
---
+++ src/dfs/python/simple/test.py
@@ -0,0 +1,22 @@
+from dfs import dfs
+
+# Smaller graph used for dijkstra
+start = {
+    "value": 0, "children": [
+        {"value": 1, "children": [
+            {"value": 3, "children": []},
+            {"value": 4, "children": []}
+        ]}, {
+            "value": 2, "children": [
+                {"value": 5, "children": []},
+                {"value": 6, "children": []}
+            ]
+        }
+    ]
+}
+
+# Bigger graph specifically for this algorithm
+# TODO
+
+# Should be 6
+print(dfs(start, 6)["value"])
