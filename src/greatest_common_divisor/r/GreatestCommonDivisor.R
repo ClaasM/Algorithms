f0e379b69973d332c17741e2d34937a8944d59d0
Claas Meiners <ga96sag@tum.de>
2019-12-20 20:30:19 +0100

    Using docstring in gcd in r

--- src/greatest_common_divisor/r/GreatestCommonDivisor.R
+++ src/greatest_common_divisor/r/GreatestCommonDivisor.R
@@ -1,12 +1,10 @@
-# Used to perform the Euclidean Algorithm to find the greatest common divisor (gcd) of two numbers.
-# For more than two numbers, e.g. three, you can box it like this: gcd(a,gcd(b,greatest_common_divisor.c)) etc.
-
-# Recursive implementation to find the gcd (greatest common divisor) of two integers using the euclidean algorithm.
-# This runs in O(log(n)) where n is the maximum of a and b.
-# @param a the first integer
-# @param b the second integer
-# @return the greatest common divisor (gcd) of the two integers.
 gcd <- function (a,b) {
+  #' Recursive implementation to find the gcd (greatest common divisor) of two integers using the euclidean algorithm.
+  #' For more than two numbers, e.g. three, you can box it like this: gcd(a,gcd(b,greatest_common_divisor.c)) etc.
+  #' This runs in O(log(n)) where n is the maximum of a and b.
+  #' @param a the first integer
+  #' @param b the second integer
+  #' @return the greatest common divisor (gcd) of the two integers.
   print(sprintf("New *a* is %s, new *b* is %s",a,b))
   if(b == 0){
     print(sprintf("b is 0, stopping recursion, a is the gcd: %s", a))
